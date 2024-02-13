6c015f579436b5dadd9bfd145d6df40ae604ba47
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-02-13 16:51:47 +0100

    Initial commit

---
+++ src/main/java/it/epicode/be/U5S2G2/U5S2G2Application.java
@@ -0,0 +1,13 @@
+package it.epicode.be.U5S2G2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S2G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S2G2Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/be/U5S2G2/U5S2G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.be.U5S2G2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S2G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
